08fdf7082860268e72855bb040c25280e26139f1
nour662 <nourali1786@gmail.com>
2022-02-21 23:50:38 -0500

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,44 +2,42 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
 
-document.addEventListener('DOMContentLoaded', () => {
-  let slidePosition = 0;
-  const slides = document.querySelectorAll('photo-grid-tiem');
-  const totalSlides = slides.length;
+let slidePosition = 0;
+const slides = document.querySelectorAll('photo-grid-tiem');
+const totalSlides = slides.length;
 
-  document.querySelector('#next-button').addEventListener('click', () => {
-    moveToNextSlide();
-  });
-  document.querySelector('#prev-button').addEventListener('click', () => {
-    moveToPrevSlide();
-  });
-
-  function updateSlidePosition() {
-    for (const slide of slides) {
-      slide.classList.remove('photo-grid-item--visible');
-      slide.classList.add('photo-grid-item--hidden');
-    }
-
-    slides[slidePosition].classList.add('photo-grid-item--visible');
-  }
-
-  function moveToNextSlide() {
-    if (slidePosition === totalSlides - 1) {
-      slidePosition = 0;
-    } else {
-      slidePosition++;
-    }
-
-    updateSlidePosition();
-  }
-
-  function moveToPrevSlide() {
-    if (slidePosition === 0) {
-      slidePosition = totalSlides - 1;
-    } else {
-      slidePosition--;
-    }
-
-    updateSlidePosition();
-  }
+document.querySelector('#next-button').addEventListener('click', () => {
+  moveToNextSlide();
 });
+document.querySelector('#prev-button').addEventListener('click', () => {
+  moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+  for (const slide of slides) {
+    slide.classList.remove('photo-grid-item--visible');
+    slide.classList.add('photo-grid-item--hidden');
+  }
+
+  slides[slidePosition].classList.add('photo-grid-item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
