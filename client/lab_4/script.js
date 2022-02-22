7c013b149afad151331ee51a72a45f0149961b65
nour662 <nourali1786@gmail.com>
2022-02-21 22:49:31 -0500

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,12 +1,40 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
 document.addEventListener('DOMContentLoaded', () => {
+  let slidePos = 0;
+  const slides = document.querySelectorAll('photo-grid-items');
+  const totalSlides = slides.length;
 
+  document.querySelector('next-button').addEventListener('click', () => {
+    moveToNext();
+  });
+  document.querySelector('prev-button').addEventListener('click', () => {
+    moveToPrev();
+  });
 
+  function updateSlidePos() {
+    for (const slide of slides) {
+      slide.classList.remove('photo_grid_item--visible');
+      slide.classList.add('photo_grid_item--hidden');
+    }
+    slides[slidePos].classList.add('photo_grid_item--visible');
+  }
 
-
-
-
-
-
-
-
-}
+  function moveToNext() {
+    if (slidePos === totalSlides - 1) {
+      slidePos = 0;
+    } else {
+      slidePos++;
+    }
+    updateSlidePos();
+  }
+  function moveToPrev() {
+    if (slidePos === 0) {
+      slidePos = totalSlides - 1;
+    } else {
+      slidePos--;
+    }
+    updateSlidePos();
+  }
+});
