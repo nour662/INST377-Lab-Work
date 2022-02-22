af5e21d5686cdf8b252a076b5ef2b8cd16c2366f
nour662 <nourali1786@gmail.com>
2022-02-22 17:02:09 -0500

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,46 +1,45 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
-document.addEventListener('DOMContentLoaded', () => {
-  let slidePosition = 0;
-  const slides = document.querySelectorAll('.photo-grid-item');
-  console.log(slides)
-  const totalSlides = slides.length;
 
-  document.querySelector('#next-button').addEventListener('click', () => {
-    moveToNextSlide();
-    console.log('move')
-  });
-  document.querySelector('#prev-button').addEventListener('click', () => {
-    moveToPrevSlide();
-  });
+let slidePosition = 0;
+const slides = document.getElementsByClassName('photo-grid-item');
+console.log(slides)
+const totalSlides = slides.length;
 
-  function updateSlidePosition() {
-    for (const slide of slides) {
-      slide.classList.remove('.photo-grid-item--visible');
-      slide.classList.add('.photo-grid-item--hidden');
-    }
-    console.log(slides[slidePosition])
-    slides[slidePosition].classList.add('.photo-grid-item--visible');
-  }
-
-  function moveToNextSlide() {
-    if (slidePosition === totalSlides - 1) {
-      slidePosition = 0;
-    } else {
-      slidePosition++;
-    }
-
-    updateSlidePosition();
-  }
-
-  function moveToPrevSlide() {
-    if (slidePosition === 0) {
-      slidePosition = totalSlides - 1;
-    } else {
-      slidePosition--;
-    }
-
-    updateSlidePosition();
-  }
+document.getElementById('next-button').addEventListener('click', () => {
+  moveToNextSlide();
+  console.log('move')
 });
+document.getElementById('prev-button').addEventListener('click', () => {
+  moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+  for (const slide of slides) {
+    slide.classList.remove('photo-grid-item--visible');
+    slide.classList.add('photo-grid-item--hidden');
+  }
+  console.log(slides[slidePosition])
+  slides[slidePosition].classList.add('photo-grid-item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
